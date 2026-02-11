04edd2670655bfcd8eeaba592789456dd425a018
Jaroslav Szkandera <jaroslav.szk@gmail.com>
2026-02-11 23:53:05 +0100

    Add flake

--- configuration.nix
+++ configuration.nix
@@ -1,71 +1,23 @@
-{ config, lib, pkgs, ... }:
-
 {
-  imports = [ ./hardware-configuration.nix ];
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [./hardware-configuration.nix];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   networking.hostName = "jarek-nixos";
   networking.networkmanager.enable = true;
+
   time.timeZone = "Europe/Prague";
   i18n.defaultLocale = "en_US.UTF-8";
 
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
-
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-  };
-
-  users.users.jarek = {
-    isNormalUser = true;
-    description = "jarek";
-    extraGroups = [ "wheel" "networkmanager" ];
-    packages = with pkgs; [
-      tree
-      htop
-      direnv
-    ];
-  };
-
-  environment.systemPackages = with pkgs; [
-    # General
-    vim neovim git rsync wget curl unzip
-
-    # Build tools
-    pkg-config
-    rustup
-    gnumake
-    gcc
-    clang
-    ccache
-    mold
-
-    # Libraries
-    fontconfig
-    fontconfig.dev
-    freetype
-    freetype.dev
-    expat
-    openssl
-    openssl.dev
-  ];
-
-  environment.variables = {
-    PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" [
-      pkgs.fontconfig.dev
-      pkgs.freetype.dev
-      pkgs.openssl.dev
-    ];
-    
-    LD_LIBRARY_PATH = lib.makeLibraryPath [
-      pkgs.fontconfig
-      pkgs.freetype
-      pkgs.expat
-      pkgs.openssl
-    ];
-  };
 
   services.openssh = {
     enable = true;
@@ -75,7 +27,53 @@
     };
   };
 
-  system.copySystemConfiguration = true;
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    auto-optimise-store = true;
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  users.users.jarek = {
+    isNormalUser = true;
+    description = "jarek";
+    extraGroups = ["wheel" "networkmanager"];
+    packages = with pkgs; [
+      tree
+      htop
+      direnv
+      neofetch
+      ripgrep
+      fd
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    # General
+    vim
+    neovim
+    git
+    rsync
+    wget
+    curl
+    unzip
+
+    # Build tools
+    rustup
+    gnumake
+    gcc
+    clang
+    ccache
+    mold
+
+    # Libraries
+    pkg-config
+    openssl
+  ];
+
   system.stateVersion = "25.11";
 }
-
---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.jarek-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
+    };
+  };
+}
